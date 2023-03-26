1454043e08eb1e3db1e2a916a7a88b255e8b243b
Fernando Nogueira <abert036@uottawa.ca>
2023-03-26 17:51:02 -0400

    wip

--- scenes/battle/battle.gd
+++ scenes/battle/battle.gd
@@ -1,11 +1,26 @@
 extends Node2D
 
+@export var playerSprite: Sprite2D;
+@export var cardPrefab: PackedScene;
+
+@export var cardsNode: Node;
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Add 4 cards to the cards node.
+	for i in range(4):
+		var card = cardPrefab.instantiate();
+		card.labelNode.text = "FooBar";
+		cardsNode.add_child(card);
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
+var elapsed = 0;
 func _process(delta):
-	pass
+	elapsed += delta;
+	if elapsed > 0.1:
+		elapsed = 0;
+		if playerSprite.modulate == Color.RED:
+			playerSprite.modulate = Color.WHITE;
+		else:
+			playerSprite.modulate = Color.RED;
